10fec2b1fe48005a67316a592a2d9ca2e8a2acca
belphegor666 <europa@localhost.localdomain>
2016-09-03 11:43:29 +0100

    step 2 - tests passed

--- src/main/java/gov/hmrc/Shop.java
+++ src/main/java/gov/hmrc/Shop.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 
 
+
 public class Shop {
 	
 	final String APPLE  = "apple";
@@ -12,9 +13,12 @@ public class Shop {
 	
 	// Initialise price catalogue
 	final HashMap<String, OrderItem> prices = new HashMap<String, OrderItem>() {{
-	    put(APPLE, new OrderItem(APPLE, 0.60d));
-	    put(ORANGE, new OrderItem(ORANGE, 0.25d));
+	    put(APPLE, new OrderItem(APPLE, 0.60d, 2, 0.00d));
+	    put(ORANGE, new OrderItem(ORANGE, 0.25d, 3, 0.00d));
 	}}; 
+	
+	private int appleCount = 0;
+	private int orangeCount = 0;
 	
 	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 	
@@ -38,11 +42,19 @@ public class Shop {
 		OrderItem item = null;
 		switch (itemName) {
 			case APPLE:
+				appleCount++;
 				item = new OrderItem(prices.get(APPLE));
+				if ((appleCount % item.getOfferFrequency()) == 0) {
+					item.setPrice(item.offerPrice);
+				}
 				orderItems.add(item);
 				break;
 			case ORANGE:
+				orangeCount++;
 				item = new OrderItem(prices.get(ORANGE));
+				if ((orangeCount % item.getOfferFrequency()) == 0) {
+					item.setPrice(item.offerPrice);
+				}
 				orderItems.add(item);
 				break;
 			default:
@@ -56,21 +68,28 @@ public class Shop {
 		private final String UNDEFINED_ITEM_NAME = "Undefined";
 		private String itemName = UNDEFINED_ITEM_NAME;
 		private double itemPrice = 0.00d;
+		private int offerFrequency = -1;
+		private double offerPrice = 0.00d;
 		
 		private OrderItem(){};
 		
-		public OrderItem(String name, double price) {
+		public OrderItem(String name, double price, int offerFrequency, double offerPrice) {
 			this.itemName = name;
 			this.itemPrice = price;
+			this.offerFrequency = offerFrequency;
+			this.offerPrice = offerPrice;
 		}
 		
 		public OrderItem(OrderItem item) {
-			this(item.getName(),item.getPrice());
+			this(item.getName(),item.getPrice(),item.getOfferFrequency(),item.getOfferPrice());
 		}
 		
 		public String getName() { return itemName; }
 		public double getPrice() { return itemPrice; }
-
+		public int getOfferFrequency() { return offerFrequency; }
+		public double getOfferPrice() { return offerPrice; }
+		
+		public void setPrice(double price) { this.itemPrice = price; }
 	}
 
 }
